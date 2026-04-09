2837c66d390efa9b8d1c690a75e3b07997770f26
BimaEkaSetiawan <bimaekasetiawan1010@gmail.com>
2026-04-09 16:03:10 +0700

    membuat method agar tersambung

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -20,7 +20,30 @@ namespace CRUDMahasiswaADO
             InitializeComponent();
             conn = new SqlConnection(connectionString);
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void ConnectDatabase()
+        {
+            try
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+
+                MessageBox.Show("Koneksi berhasil");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Koneksi gagal: " + ex.Message);
+            }
+        }
+
        
+
 
     }
 }
